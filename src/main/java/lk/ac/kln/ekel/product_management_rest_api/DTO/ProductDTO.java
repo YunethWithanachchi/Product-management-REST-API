c80addc4652fdbded6554ad4c07ae0b6914e2567
Yuneth Withanachchi <yunethwithanachchi03@gmail.com>
2025-12-27 21:36:40 +0530

    testing whether intellij could make repos

---
+++ src/main/java/lk/ac/kln/ekel/product_management_rest_api/DTO/ProductDTO.java
@@ -0,0 +1,5 @@
+package lk.ac.kln.ekel.product_management_rest_api.DTO;
+
+public class ProductDTO {
+
+}
---
+++ src/test/java/lk/ac/kln/ekel/product_management_rest_api/ProductManagementRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ac.kln.ekel.product_management_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagementRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
