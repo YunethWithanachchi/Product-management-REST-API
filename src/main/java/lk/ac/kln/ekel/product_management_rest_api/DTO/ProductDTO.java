05e49fe5352e5ad56f177370a552b5f0bd58a179
Yuneth Withanachchi <yunethwithanachchi03@gmail.com>
2025-12-27 21:43:22 +0530

    Entity created

--- src/main/java/lk/ac/kln/ekel/product_management_rest_api/DTO/ProductDTO.java
+++ src/main/java/lk/ac/kln/ekel/product_management_rest_api/DTO/ProductDTO.java
@@ -1,5 +1,22 @@
 package lk.ac.kln.ekel.product_management_rest_api.DTO;
 
-public class ProductDTO {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "Product")
+public class ProductDTO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    String productId;
+    String productName;
+    String category;
+    Double price;
+    int quantity;
 }
